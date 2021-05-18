2a4913f97600b9212cf4d22511a30c59f06ed387
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2021-05-19 00:08:24 +0530

    commit on master

---
+++ problems_on_numbers/gcd.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cin>>num1>>num2;
+    while(1)
+    {
+        if(num1 < num2)
+        {
+            swap(num1, num2);
+        }
+
+        // num1 -= num2;          Not optimized
+
+        num1 = num1 % num2;    // Optimized
+        if(num1 == 0)
+        {
+            cout<<num2;
+            break;
+        }
+    }
+}
---
+++ problems_on_numbers/pascal_triangle.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(j <= i)
+            {
+                if(j == 0 || i==j)
+                    a[i][j] = 1;
+                else
+                    a[i][j] = a[i-1][j-1] + a[i-1][j];
+                cout<<a[i][j]<<"\t";
+            }
+            else
+                a[i][j] = 0;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}

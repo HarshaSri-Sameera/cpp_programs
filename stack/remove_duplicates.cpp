2838662e63d83562653fdf32f4ebe9987b0dcba3
Harsha Sri Sameera <70195328+HarshaSri-Sameera@users.noreply.github.com>
2022-04-25 18:54:04 +0530

    Create remove_duplicates.cpp

---
+++ stack/remove_duplicates.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> stack_fun(int arr[])
+{
+    vector<int> v;
+    int n = 10;
+    stack<int> st;
+    st.push(arr[0]);
+    
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]!=st.top())
+        {
+            v.push_back(st.top());
+            st.push(arr[i]);
+        }
+    }
+    
+    return v;
+}
+
+// To remove the duplicates using stack data structure
+int main()
+{
+    int arr[] = {5, 8, 9, 4, 8, 9, 4, 6, 2, 3};
+    int n = 10;
+    sort(arr, arr+n);
+    vector<int> v;
+    v = stack_fun(arr);
+    
+    for(auto it: v)
+    {
+        cout<<it<<" ";
+    }
+    return 0;
+}
